e2e3ee99c1c3099102356aa013545370b1c58cb1
Max <max.edison@yahoo.com>
2021-11-24 15:24:53 -0500

    commit tree f0fb1f8cdc58b5088468b79260f294769ebf930f

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
+    # if self.pop =="."
+    #   true
+    # else
+    #   false
+    # end
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self_arr = self.split(/[!?.]/)
+    # binding.pry
+    self_arr.count{|str| str.length>0}
   end
 end
